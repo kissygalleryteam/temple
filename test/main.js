f048da0572b477cc9ee9116abe3dd65a7341a672
cookieu <cookieu@gmail.com>
2013-09-03 23:52:43 +0800

    基本实现

--- test/main.js
+++ test/main.js
@@ -1,1 +1,13 @@
-new RegExp("|")
+var regvar = /[a-zA-Z_$][a-zA-Z_$0-9]*/g;
+//
+var s = "_aa bb ab abc (bar > foo)";
+var r;
+var vars = [];
+while((r = regvar.exec(s))){
+  //console.log(r[0]);
+}
+s = s.replace(regvar,function(m,a){
+  //console.log(m);
+  return "env."+m;
+})
+console.log(s);
--- test/parser-test.js
+++ test/parser-test.js
@@ -43,117 +43,170 @@ var result
 // console.log(result[0])
 
 
-result = parser.parse(fs.readFileSync("./templates/if6.txt").toString());
-
+result = parser.parse(fs.readFileSync("./templates/simple.txt").toString());
 var ast = [];
 var next = result[0].next;
-
-function walk(next){
-  var list = [];
-  //-------------------- each --------------------
-  var eachComprehension;
-  var eachBody;
-  //-------------------- each --------------------
-
-  //-------------------- if   --------------------
-  var ifPredict;
-  var ifYes;
-  var ifElse;//else
-
-  var ifExpression;//包含ifPredict ifYes ifNo
-  var ifExpressions;
-  //-------------------- if   --------------------
-
-  //-------------------- op   --------------------
-  var opcache;
-  //-------------------- op   --------------------
-
-  //当前容器
-  var pointer = ast;
-  while(next){
-    if(next.name == "@eachstart"){
-      list = ["each"];
-    }else if(next.name == "@eachend"){
-      ast.push(list);
-      pointer = ast;
-    }else if(next.name == "@each-head-start"){
-      eachComprehension = [];
-    }else if(next.name == "@each-head-end"){
-      list.push(eachComprehension);
-    }else if(next.name == "@each-body-start"){
-      eachBody = [];
-      pointer = eachBody;
-    }else if(next.name == "@each-body-end"){
-      list.push(eachBody);
-    }else if(next.name == "each-items"){
-      eachComprehension.push(next.value);
-      pointer = eachComprehension;
-    }else if(next.name == "each-item"){
-      eachComprehension.push(next.value);
-    }else if(next.name == "each-index"){
-      eachComprehension.push(next.value);
-    }else if(next.name == "@ifstart"){
-      list = ["if"];
-      ifExpressions = [];
-      ifExpression = [];
-      ifPredict = [];
-      pointer = ifPredict;
-    }else if(next.name == "@ifend"){
-      // ifExpression.push(ifPredict);
-
-      list.push(ifExpressions);
-      //若有else clause 的话
-      ifElse.length && list.push(ifElse);
-      add(list,ast);
-
-      // resetting;
-      pointer = ast;
-    }else if(next.name == "@elseifstart"){
-      ifExpression = [];
-      ifPredict = [];
-      pointer = ifPredict;
-    }else if(next.name == "@elseifend"){
-      //do nothing
-    }else if(next.name == "expression"){
-      add(["expression",next.value]);
-    }else if(next.name == "@ifbodystart"){
-      ifYes = [];
-      pointer = ifYes;
-    }else if(next.name == "@ifbodyend"){
-      ifExpression.push(ifPredict);
-      ifExpression.push(ifYes);
-      ifExpressions.push(ifExpression);
-    }else if(next.name == "@elsestart"){
-      ifElse = [];
-      pointer = ifElse;
-    }else if(next.name == "@elseend"){
-    }else if(next.name == "var"){
-      add(["var",next.value]);
-    }else if(next.name == "number"){
-      add(["number",next.value]);
-    }else if(next.name == "string"){
-      if(next.value == "\n"){
-        add(["string",'"\\n"']);
-      }else{
-        var strs = next.value.split("\n");
-        for(var i=0,l = strs.length;i<l;i++){
-          if(strs[i]){
-            add(["string",'"\\n"']);
-            add(["string",strs[i]]);
-          }
+function gen(next,prev,list){
+  next.name && console.log(next.name);
+  // console.log(list);
+  var name = next.name;
+  if(name == "@ifend" || name == "@ifexpend" || name == "@ifbodyend" || name == "@elseifend" || name == "@elseend"){
+    list = prev;
+  }else if(next.name == "@eachstart"){
+    var eachclause = ["each"];
+    list.push(eachclause);
+    prev = list;
+    list = eachclause;
+  }else if(next.name == "@each-head-start"){
+    var eachComprehension = [];
+    var eachBracket = [eachComprehension];
+    list.push(eachBracket);
+    prev = list;
+    list = eachComprehension;
+  }else if(next.name == "@each-body-start"){
+    var eachBody = [];
+    list.push(eachBody);
+    prev = list;
+    list = eachBody;
+  }else if(next.name == "each-items"){
+    list.push(next.value);
+  }else if(next.name == "each-item"){
+    list.push(next.value);
+  }else if(next.name == "each-index"){
+    list.push(next.value);
+  }else if(next.name == "@ifstart"){
+    var ifexpressions = [];
+    var ifclause = ["if",ifexpressions];
+    list.push(ifclause);
+    prev = list;
+    list = ifclause;
+  }else if(next.name == "@elseifstart"){
+    var ifexpression = [];
+    list.push(ifexpression);
+    prev = list;
+    list = ifexpression;
+  }else if(next.name == "@ifexpstart"){
+    var predict = [];
+    list.push(predict);
+    prev = list;
+    list = predict;
+  }else if(next.name == "@ifbodystart"){
+    var yesexp = [];
+    list.push(yesexp);
+    prev = list;
+    list = yesexp;
+  }else if(next.name == "expression"){
+    list.push(next.value);
+  }else if(next.name == "number"){
+    list.push(["number",next.value]);
+  }else if(next.name == "string"){
+    if(next.value == "\n"){
+      list.push(["string",'"\\n"']);
+    }else{
+      var strs = next.value.split("\n");
+      for(var i=0,l = strs.length;i<l;i++){
+        if(strs[i]){
+          list.push(["string",'"\\n"']);
+          list.push(["string",strs[i]]);
         }
       }
     }
-    next = next.next;
   }
-
-  function add(list,con){
-    var container = con || pointer || ast;
-    container.push(list);
-  }
+  next = next.next;
+  next && gen(next,prev,list);
 }
-walk(next);
+//gen(next,null,ast);
+
+function gen(next,currentContext,ctxq){
+  // next.name && console.log(next.name);
+  // console.log("ctxq.length=",ctxq.length);
+  var name = next.name;
+  if(name == "@ifend" || name == "@ifbodyend" || name == "@elseifend" || name == "@elseend"){
+    if(ctxq.length){
+      currentContext = ctxq.pop();
+    }
+  }else if(next.name == "@eachstart"){
+    var eachclause = ["each"];
+    currentContext.push(eachclause);
+    ctxq.push(currentContext);
+    currentContext = eachclause;
+  }else if(next.name == "@each-head-start"){
+    var eachComprehension = [];
+    currentContext.push(eachComprehension);
+    ctxq.push(currentContext);
+    currentContext = eachComprehension;
+  }else if(next.name == "@each-body-start"){
+    currentContext = ctxq.pop();
+
+    var eachBody = [];
+    currentContext.push(eachBody);
+    ctxq.push(currentContext);
+    currentContext = eachBody;
+  }else if(next.name == "each-items"){
+    currentContext.push(next.value);
+  }else if(next.name == "each-item"){
+    currentContext.push(next.value);
+  }else if(next.name == "each-index"){
+    currentContext.push(next.value);
+  }else if(next.name == "@ifstart"){
+    var ifexpressions = [];
+    var ifclause = ["if",ifexpressions];
+    currentContext.push(ifclause);
+    //存储两级context
+    ctxq.push(currentContext);
+    ctxq.push(ifclause);
+    currentContext = ifexpressions;
+  }else if(next.name == "@elseifstart"){
+    var ifexpression = [];
+    currentContext.push(ifexpression);
+    ctxq.push(currentContext);
+    currentContext = ifexpression;
+  }else if(next.name == "@ifexpstart"){
+    var predict = [];
+    currentContext.push(predict);
+    ctxq.push(currentContext);
+    currentContext = predict;
+  }else if(name == "@elsestart"){
+    currentContext = ctxq.pop();
+    var elseexp = [];
+    currentContext.push(elseexp);
+    ctxq.push(currentContext);
+    currentContext = elseexp;
+  }else if(name == "@ifexpend"){
+    if(ctxq.length){
+      currentContext = ctxq.pop();
+    }
+  }else if(next.name == "@ifbodystart"){
+    var yesexp = [];
+    currentContext.push(yesexp);
+    ctxq.push(currentContext);
+    currentContext = yesexp;
+  }else if(next.name == "expression"){
+    currentContext.push(next.value);
+  }else if(next.name == "var"){
+    currentContext.push(["var",next.value]);
+  }else if(next.name == "number"){
+    currentContext.push(["number",next.value]);
+  }else if(next.name == "string"){
+    if(next.value == "\n"){
+      currentContext.push(["string",'\\n']);
+    }else{
+      var strs = next.value.split("\n");
+      for(var i=0,l = strs.length;i<l;i++){
+        if(strs[i]){
+          currentContext.push(["string",'\\n']);
+          currentContext.push(["string",strs[i]]);
+        }
+      }
+    }
+  }
+  next = next.next;
+  next && gen(next,currentContext,ctxq);
+}
+gen(next,ast,[]);
 // console.log(JSON.stringify(ast))
 console.log(jit.to_js(ast));
-// console.log(jit.compile(ast));
+var tpl = jit.compile(ast);
+var ret = tpl.render({name:"tom"});
+console.log(ret);
 

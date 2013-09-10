fa1b472027a51dac1749d884dffdc34222b3ebad
cookieu <cookieu@gmail.com>
2013-09-10 22:59:11 +0800

    文件夹调整

--- test/compile2vm.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-var parser = require("../lib/parser");
-var compiler = require("../lib/compile2vm");
-
-var ast = parser.toAST(fs.readFileSync("./templates/simple.txt").toString());
-console.log(compiler.to_vm(ast));
-

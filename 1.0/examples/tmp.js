cc71ddd7a86c8423c172be50de2e6077313b3a67
yuanhuang <yuanhuang.tjw@taobao.com>
2013-09-24 19:35:01 +0800

    文档示例

--- 1.0/examples/tmp.js
+++ 1.0/examples/tmp.js
@@ -1,28 +1,3 @@
-## Temple 模板引擎 (线上环境慎用)
-
-### Features
-- if/elseif/else
-- each
-- include 子模板
-- extend 模板继承
-- if 表达式支持嵌套
-- 自定义函数
-- 编译后的代码非常直观,不信你看 
-- 语法类似KISSY 1.2 Template ,不过少了个花括号
-
-### 用法
-
-```javascript
-KISSY.use('gallery/temple/1.0/index', function (S, Temple) {
-     Temple.add("base","this is head , my name is {#block name} jerry {/block}!");
-     var temple = Temple.compile('{#extend base} {#block name} john {/block}'));
-	 var html = temple.render({name:"Tom"});
-	 console.log(html);
-})
-```
-### 支持语法示例
-
-```javascript
 //-------------------- if --------------------
 
   '{#if name}'
@@ -94,8 +69,4 @@ var temple  = Temple.compile('{myescape(htmlstr)}');
 '{#! 可以包含"{"、"#"、"\\}" }';
 
 '{#! 中文注释} 中文字符串abc';
-
-```
-
-## Licence
 
